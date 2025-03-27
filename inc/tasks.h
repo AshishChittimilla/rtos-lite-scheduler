129f616a35b0404a77a101d84ed9bc3cfbcc06fc
Ashish Chittimilla <chittimilla_ashish@hotmail.com>
2025-03-27 16:13:42 +0530

    Add files via upload

---
+++ inc/tasks.h
@@ -0,0 +1,11 @@
+
+#ifndef TASKS_H
+#define TASKS_H
+
+// Declaration of task functions
+void LED_Task(void);
+void UART_Task(void);
+
+// Add your additional task declarations here
+
+#endif // TASKS_H
